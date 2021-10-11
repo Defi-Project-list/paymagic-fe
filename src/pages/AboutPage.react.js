041cbaba7110b5b15ddad630094a26f9b765d697
Corbin Page <corbin.page@gmail.com>
2021-10-11 16:25:58 -0400

    Edited About Page and hid Transaction page

--- src/pages/AboutPage.react.js
+++ src/pages/AboutPage.react.js
@@ -24,14 +24,14 @@ function About() {
               statusColor="blue"
               body={(
                 <p style={{whiteSpace: "pre-line"}}>
-Paymagic empowers DAOs and crypto teams to send specialized crypto payments, like batch transfers, vesting schedules, streaming, escrow, airdrops, etc, on any EVM compatible blockchain or Layer 2’s. 💸✨{"\n\n"}
-The app was inspired by the ideas and creations of projects like Disperse.app, SuperFluid, Sabler, MerkleDrops, and many many more. 🙏 More details about Paymagic can be found <a target="_blank" href="https://launch.mirror.xyz/tkvx9MAcsuSag0l5fa_7CmRTKyPSlyRtrMHulwknMUw">on Mirror here</a>. We'll plan to ship updates, new features, and new networks in the coming months.{"\n\n"}
+Paymagic is a payment tool for DAOs and crypto teams. Send batch token transfers, airdrops, vesting schedules, streaming payments, escrow, and more on any blockchain or Layer 2’s. 💸✨{"\n\n"}
+The app was inspired by the ideas and creations of projects like Disperse.app, SuperFluid, Sabler, MerkleDrops, and many more. 🙏 {"\n\n"}
 
-If you have bugs to report, feedback, or feature requests, please <a target="_blank" href="https://airtable.com/shrpR5auT6RUIOrDC">submit them here</a>.🐛{"\n\n"}
+🐛 Submit <a target="_blank" href="https://airtable.com/shrpR5auT6RUIOrDC">bugs or feature requests here.</a>.{"\n"}
+💬 To contact or contribute to the DAO, <a target="_blank" href="https://t.me/paymagic">join on Telegram</a>.{"\n"}
+🪞 More details can be <a target="_blank" href="https://launch.mirror.xyz/tkvx9MAcsuSag0l5fa_7CmRTKyPSlyRtrMHulwknMUw"> found on Mirror</a>.{"\n"}
 
-If you'd like to join the DAO and contribute to the project, <a target="_blank" href="https://t.me/paymagic">join the Telegram group here</a>.💬{"\n\n"}
-
-Cheers,{"\n"}
+{"\n"}Cheers,{"\n"}
 ✨ 💸 ✨ Paymagic Team ✨ 💸 ✨
                 </p>
                 )}
